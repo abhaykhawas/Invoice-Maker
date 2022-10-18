2c9612233ec5f9de932896c588e46c3dd7c27994
Abhay <abhaykhawas@gmail.com>
2022-10-18 15:04:23 +0530

    Add session storage facility

--- script.js
+++ script.js
@@ -3,6 +3,28 @@ const formEl = document.querySelector("form");
 
 formEl.addEventListener("submit", onAddWebsite);
 
+for (var i=0; i<sessionStorage.length;i++){
+  if(!sessionStorage.key(i).indexOf('obj')){
+    x = sessionStorage.getItem(sessionStorage.key(i))
+    console.log(JSON.parse(x).pn)   
+    
+    tbodyEl.innerHTML += `
+            <tr>
+                <td>${JSON.parse(x).pn}</td>
+                <td>${JSON.parse(x).qty}</td>
+                <td>${JSON.parse(x).rate}</td>
+                <td>${JSON.parse(x).amt}</td>
+                <td>${JSON.parse(x).dis}</td>
+                <td>${JSON.parse(x).total}</td>
+                <td>${JSON.parse(x).cgst}</td>
+                <td>${JSON.parse(x).sgst}</td>
+                <td>${JSON.parse(x).famt}</td>
+            </tr>
+        `;
+  }
+}
+
+
 function onAddWebsite(e){
   e.preventDefault();
   const pn = document.getElementById('pn').value;
@@ -14,6 +36,20 @@ function onAddWebsite(e){
   const cgst = total*0.09
   const sgst = cgst
   const famt = total + cgst + sgst
+  nums = sessionStorage.length
+  named = "obj"+nums
+  sessionStorage.setItem(named,JSON.stringify({
+    "pn":pn,
+    "qty":qty,
+    "rate":rate,
+    "amt":amt,
+    "dis":dis,
+    "total":total,
+    "cgst":cgst,
+    "sgst":sgst,
+    "famt":famt
+  }))
+  
           tbodyEl.innerHTML += `
             <tr>
                 <td>${pn}</td>
